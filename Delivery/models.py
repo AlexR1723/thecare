d56bc4d90f9ffe2664790d631c72631353f571e0
avdeenko.aleksey@yandex.com <grinya22041982>
2020-01-07 20:41:25 +0300

    delivery 1

--- Delivery/models.py
+++ Delivery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Delivery(models.Model):
+    text = models.CharField(max_length=500, blank=True, null=True, verbose_name='Текст')
+
+    class Meta:
+        managed = False
+        db_table = 'delivery'
+        verbose_name = "Доставка"
+        verbose_name_plural = "Доставки"
--- Delivery/views.py
+++ Delivery/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
-def Delivery(request):
+def Deliveries(request):
+    list = Delivery.objects.all()
     return render(request, 'Delivery/Delivery.html', locals())
