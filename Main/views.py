20e9714bb158e45abd246359b67cf4e8c5a0cc2f
Anastasiya <romanenko.anastasiya64@gmail.com>
2020-06-11 14:47:11 +0300

    url

--- Main/views.py
+++ Main/views.py
@@ -109,7 +109,7 @@ def Save_excel_file(request):
             file = Files(file=doc['excel-file'])
             file.save()
             print(file.file.url)
-            rb = xlrd.open_workbook(settings.MEDIA_ROOT + file.file.url)
+            rb = xlrd.open_workbook(file.file.url)
             sheet = rb.sheet_by_index(0)
             vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
             for v in vals:
