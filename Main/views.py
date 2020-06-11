458445821808b8ff3642f93c4c6b8741b169f3cf
Anastasiya <romanenko.anastasiya64@gmail.com>
2020-06-11 15:59:47 +0300

    чтение файла

--- Main/views.py
+++ Main/views.py
@@ -115,8 +115,8 @@ def Save_excel_file(request):
             # rb = xlrd.open_workbook(default_storage.location + file.file.name)
             # url = default_storage.url(file.file.name)
             # rb = xlrd.open_workbook(settings.MEDIA_ROOT + '/' + file.file.name)
-            print(file.file.name)
-            wb = load_workbook(file.file.name)
+            print(settings.MEDIA_ROOT + '/' + file.file.name)
+            wb = load_workbook(settings.MEDIA_ROOT + '/' + file.file.name)
             # rb = xlrd.open_workbook(url)
             # sheet = rb.sheet_by_index(0)
             # vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
