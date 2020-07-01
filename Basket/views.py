f33c00e97c525015fb8571969e6cae08eebd298b
Alex <avdeenko.aleksey@yandex.com>
2020-07-01 11:21:56 +0300

    pay 2

--- Basket/views.py
+++ Basket/views.py
@@ -1,6 +1,4 @@
-import datetime
-import json
-import random
+import datetime, json, random, hashlib
 
 from django.conf import settings
 from django.http import HttpResponse
@@ -224,8 +222,48 @@ def buy_products(request):
         return HttpResponse(json.dumps(True))
 
 
+# записывать заказ в бд
+# проверять заказ по номеру
+# сверстать страницу оплаты
+# создать таблицу в бд с товарами ???
 def pay_result(request):
+    # $out_summ = $_REQUEST["OutSum"];
+    # $inv_id = $_REQUEST["InvId"];
+    # $shp_item = $_REQUEST["Shp_item"];
+    # $crc = $_REQUEST["SignatureValue"];
+    #
+    # $crc = strtoupper($crc);
+    #
+    # $my_crc = strtoupper(md5("$out_summ:$inv_id:$mrh_pass2:Shp_item=$shp_item"));
+
+    # summ=request.GET.get('OutSum')
+    summ = '5656556'
+    login = request.GET.get('InvId')
+    hash = str(request.GET.get('SignatureValue')).upper()
+    hs = summ + settings.PAY_INV + settings.PAY_TEST_PASSWORD_2
+    new_hash = hashlib.md5(hs.encode()).hexdigest()
+    # hash = hashlib.md5(nm.encode())
+    # print(settings.PAY_INV)
+    print(new_hash)
+    print('create_hash')
+    print(create_hash(request))
     return HttpResponse(json.dumps('OK'))
+
+
+def create_hash(request):
+    # summ=request.GET.get('OutSum')
+    summ = '5656556'
+    hs = settings.PAY_LOGIN + ':' + summ + ':' + settings.PAY_INV + ':' + settings.PAY_TEST_PASSWORD_1
+    print(hs)
+    user=get_user_id(request)
+    if user:
+        hs+=':user='+str(user)
+    else:
+        hs+=':user=0'
+    print(hs)
+    new_hash = hashlib.md5(hs.encode()).hexdigest()
+    print(new_hash)
+    return True
 
 
 def pay_success(request):
